cb5dc8c884e65bf9636da50d40455a546fa2369c
LuizFernando55 <45644952+LuizFernando55@users.noreply.github.com>
2020-06-28 18:39:29 -0300

    Semana Finalizada

---
+++ scripts/load.js
@@ -0,0 +1,15 @@
+function preload() {
+    imagemCenario = loadImage('imagens/cenario/floresta.png');
+    imagemGameOver = loadImage('imagens/assets/game-over.png');
+    imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+    imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
+    imagemInimigoGrande = loadImage('imagens/inimigos/troll.png');
+    imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
+    imagemTelaInicial = loadImage('imagens/cenario/telaInicial.png');
+    imagemVida = loadImage('imagens/assets/coracao.png');
+
+    fita = loadJSON('fita/fita.json');
+    fonteTelaInicial = loadFont('imagens/assets/fonteTelaInicial.otf');
+    somDoJogo = loadSound('sons/trilha_jogo.mp3');
+    somDoPulo = loadSound('sons/somPulo.mp3');
+  }
